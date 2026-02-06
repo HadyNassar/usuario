d1ac5a8c338345ab05cf042968e587bbcc8ae3d4
HadyNassar <Hadynassar02@gmail.com>
2026-02-05 20:51:44 -0400

    adicionado metodo que salva dados de usuario

---
+++ src/main/java/com/hady/usuario/business/UsuarioService.java
@@ -0,0 +1,24 @@
+package com.hady.usuario.business;
+
+import com.hady.usuario.business.converter.UsuarioConverter;
+import com.hady.usuario.business.dto.UsuarioDTO;
+import com.hady.usuario.infrastructure.entity.Usuario;
+import com.hady.usuario.infrastructure.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final UsuarioConverter usuarioConverter;
+
+    public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+       Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
+       return usuarioConverter.paraUsuarioDTO(
+               usuarioRepository.save(usuario));
+    }
+
+
+}
