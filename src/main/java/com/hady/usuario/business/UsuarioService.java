12c54530a3f3ce574001c7393240219a3f83f17d
HadyNassar <Hadynassar02@gmail.com>
2026-02-06 17:04:57 -0400

    adicionados metodos get, delete e login

--- src/main/java/com/hady/usuario/business/UsuarioService.java
+++ src/main/java/com/hady/usuario/business/UsuarioService.java
@@ -3,8 +3,11 @@ package com.hady.usuario.business;
 import com.hady.usuario.business.converter.UsuarioConverter;
 import com.hady.usuario.business.dto.UsuarioDTO;
 import com.hady.usuario.infrastructure.entity.Usuario;
+import com.hady.usuario.infrastructure.exceptions.ConflictException;
+import com.hady.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.hady.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,12 +16,39 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+        emailExiste(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
        Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
        return usuarioConverter.paraUsuarioDTO(
                usuarioRepository.save(usuario));
     }
 
+    public void emailExiste(String email){
+        try{
+            boolean existe = verificaEmailExistente(email);
+            if(existe){
+                throw new ConflictException("email ja cadastrado" + email);
+            }
+        }catch (ConflictException e){
+            throw new ConflictException("Email ja cadastrado." + e.getCause());
+        }
+    }
+
+
+    public boolean verificaEmailExistente(String email) {
+        return usuarioRepository.existsByEmail(email);
+    }
+
+    public Usuario buscarUsuarioPorEmail (String email){
+        return usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email, não encontrado"+ email));
+    }
+    public void deletaUsuarioPorEmail(String email){
+        usuarioRepository.deleteByEmail((email));
+
+    }
 
 }
--- src/main/java/com/hady/usuario/controller/UsuarioController.java
+++ src/main/java/com/hady/usuario/controller/UsuarioController.java
@@ -2,12 +2,13 @@ package com.hady.usuario.controller;
 
 import com.hady.usuario.business.UsuarioService;
 import com.hady.usuario.business.dto.UsuarioDTO;
+import com.hady.usuario.infrastructure.entity.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
@@ -15,8 +16,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final com.hady.usuario.infrastructure.security.JwtUtil jwtUtil;
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO){
         return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
     }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDTO usuarioDTO) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(),
+                        usuarioDTO.getSenha())
+        );
+        return "Bearer" + jwtUtil.generateToken(authentication.getName());
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscaUsuarioPorEmail(@RequestParam("email")
+                                                        String email) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deletaUsuarioPorEmail(@PathVariable String email) {
+        usuarioService.deletaUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+    }
+
+
+
 }
